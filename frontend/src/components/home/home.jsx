c9ef096159d60a977926271354dd08880a3b6a67
Saumyaa Prajapat <saumyaa.prajapat@gamil.com>
2023-12-18 23:27:52 +0530

    Authentication

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -8,11 +8,11 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get("https://hub4-back.vercel.app/home")
+      .get("https://hub4-back.vercel.app/home", { withCredentials: true })
       .then((result) => {
         console.log(result);
         if (result.data !== "Success") {
-          //navigate("/login");
+          navigate("/login");
         }
       })
       .catch((err) => console.log(err));
