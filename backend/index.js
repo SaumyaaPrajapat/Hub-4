80927ee67d99af0db2a403836a75d737bec4c0d2
Saumyaa Prajapat <saumyaa.prajapat@gamil.com>
2023-12-23 22:39:04 +0530

    backend routes

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = 4001;
 
 app.use(cors());
+app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/employee", employeeRoutes);
 app.use("/category", categoryRoutes);
